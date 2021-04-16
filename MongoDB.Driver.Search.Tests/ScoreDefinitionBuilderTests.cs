776cf873c04d43d90b5533da668655ed7305946f
David Golub <david.golub@mongodb.com>
2021-04-16 15:23:52 -0400

    Add BoostValueScoreDefinition and BoostPathScoreDefinition

---
+++ MongoDB.Driver.Search.Tests/ScoreDefinitionBuilderTests.cs
@@ -0,0 +1,73 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using Xunit;
+
+namespace MongoDB.Driver.Search.Tests
+{
+    public class ScoreDefinitionBuilderTests
+    {
+        [Fact]
+        public void Boost()
+        {
+            var subject = CreateSubject<BsonDocument>();
+
+            AssertRendered(
+                subject.Boost(1),
+                "{ boost: { value: 1 } }");
+            AssertRendered(
+                subject.Boost("x"),
+                "{ boost: { path: 'x' } }");
+            AssertRendered(
+                subject.Boost("x", 1),
+                "{ boost: { path: 'x', undefined: 1 } }");
+        }
+
+        [Fact]
+        public void Boost_Typed()
+        {
+            var subject = CreateSubject<Person>();
+
+            AssertRendered(
+                subject.Boost(x => x.Age),
+                "{ boost: { path: 'age' } }");
+            AssertRendered(
+                subject.Boost("Age"),
+                "{ boost: { path: 'age' } }");
+        }
+
+        [Fact]
+        public void Constant()
+        {
+            var subject = CreateSubject<BsonDocument>();
+
+            AssertRendered(
+                subject.Constant(1),
+                "{ constant: { value: 1 } }");
+        }
+
+        private void AssertRendered<TDocument>(ScoreDefinition<TDocument> score, string expected)
+        {
+            AssertRendered(score, BsonDocument.Parse(expected));
+        }
+
+        private void AssertRendered<TDocument>(ScoreDefinition<TDocument> score, BsonDocument expected)
+        {
+            var documentSerializer = BsonSerializer.SerializerRegistry.GetSerializer<TDocument>();
+            var renderedQuery = score.Render(documentSerializer, BsonSerializer.SerializerRegistry);
+
+            Assert.Equal(expected, renderedQuery);
+        }
+
+        private ScoreDefinitionBuilder<TDocument> CreateSubject<TDocument>()
+        {
+            return new ScoreDefinitionBuilder<TDocument>();
+        }
+
+        private class Person
+        {
+            [BsonElement("age")]
+            public int Age { get; set; }
+        }
+    }
+}
--- MongoDB.Driver.Search/ScoreDefinitionBuilder.cs
+++ MongoDB.Driver.Search/ScoreDefinitionBuilder.cs
@@ -1,16 +1,71 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using MongoDB.Driver.Core.Misc;
+using System;
+using System.Linq.Expressions;
 
 namespace MongoDB.Driver.Search
 {
     public sealed class ScoreDefinitionBuilder<TDocument>
     {
+        public ScoreDefinition<TDocument> Boost(double value)
+        {
+            return new BoostValueScoreDefinition<TDocument>(value);
+        }
+
+        public ScoreDefinition<TDocument> Boost(PathDefinition<TDocument> path, double undefined = 0)
+        {
+            return new BoostPathScoreDefinition<TDocument>(path, undefined);
+        }
+
+        public ScoreDefinition<TDocument> Boost(Expression<Func<TDocument, double>> path, double undefined = 0)
+        {
+            return Boost(new ExpressionFieldDefinition<TDocument>(path), undefined);
+        }
+
         public ScoreDefinition<TDocument> Constant(double value)
         {
             return new ConstantScoreDefinition<TDocument>(value);
         }
     }
 
+    internal class BoostValueScoreDefinition<TDocument> : ScoreDefinition<TDocument>
+    {
+        private readonly double _value;
+
+        public BoostValueScoreDefinition(double value)
+        {
+            _value = EnsureExtensions.IsGreaterThanZero(value, nameof(value));
+        }
+
+        public override BsonDocument Render(IBsonSerializer<TDocument> documentSerializer, IBsonSerializerRegistry serializerRegistry)
+        {
+            var document = new BsonDocument("value", _value);
+            return new BsonDocument("boost", document);
+        }
+    }
+
+    internal class BoostPathScoreDefinition<TDocument> : ScoreDefinition<TDocument>
+    {
+        private readonly PathDefinition<TDocument> _path;
+        private readonly double _undefined;
+
+        public BoostPathScoreDefinition(PathDefinition<TDocument> path, double undefined)
+        {
+            _path = Ensure.IsNotNull(path, nameof(path));
+            _undefined = undefined;
+        }
+
+        public override BsonDocument Render(IBsonSerializer<TDocument> documentSerializer, IBsonSerializerRegistry serializerRegistry)
+        {
+            var document = new BsonDocument("path", _path.Render(documentSerializer, serializerRegistry));
+            if (_undefined != 0)
+            {
+                document.Add("undefined", _undefined);
+            }
+            return new BsonDocument("boost", document);
+        }
+    }
     internal sealed class ConstantScoreDefinition<TDocument> : ScoreDefinition<TDocument>
     {
         private readonly double _value;
